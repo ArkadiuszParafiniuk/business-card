28a9996e6396923516a50a83d60c2920249acaf2
s-301-12 <s-301-12@gumiforest.uek.krakow.pl>
2019-04-12 20:34:22 +0200

    test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,26 +5,3 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 }
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function infoAboutMe() {
-
-	info =  'Hi, I am your Arek :-)';
-
-	navigator.notification.alert(info);
-	
-}
